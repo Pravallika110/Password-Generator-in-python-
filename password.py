7c143dc5d5a6de7a608f1541210e0fd25a4f5d40
Pravallika110 <pravallika.33.a@gmail.com>
2024-07-04 12:20:49 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+len=int(input("enter the lenght of the password"))
+
+upper = string.ascii_uppercase
+lower = string.ascii_lowercase
+num = string.digits
+symbols = string.punctuation
+
+all=upper+lower+num+symbols
+
+temp = random.sample(all,len)
+password = "".join(temp)
+print(password)
